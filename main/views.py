7607d32bdee9554a421cc7aac584b29fc712e8d8
Gussuup <egrya13@gmail.com>
2023-04-17 14:17:06 +0800

    SyntaxErr (venv?)

--- main/views.py
+++ main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse('<h4>Hello</h4>')
+    return render(request, 'main/index.html')              #Объединяет заданный шаблон с заданным контекстным словарем и возвращает
+                                            #объект HttpResponse с этим визуализированным кодом
+
+def about(request):
+    return HttpResponse('<h4>About</h4>')
