e8512feb227798b07aa8e899d94614589fe491de
Gussuup <egrya13@gmail.com>
2023-04-24 10:28:24 +0800

    Create page added, table display added

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('Second', views.about)
+    path('second', views.about),
+    path('create', views.create, name='create')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 def index(request):
     tasks = Task.objects.all()                                                                                          #помещение в переменную всех объектов из модели Task
@@ -9,4 +10,21 @@ def index(request):
 
 def about(request):
     return render(request, 'main/about.html')
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Wrong form content'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
 
